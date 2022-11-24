8c52aadd49016541995372632d4d0c57e741ba39
PaulBarron <paul.barron@attabotics.com>
2022-11-24 16:34:04 -0700

    Initial commit after following Sagatowski tutorial

---
+++ AdsClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using TwinCAT.Ads;
+
+namespace AdsClientApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            using (AdsClient client = new AdsClient())
+            {
+                int valueToRead = 0;
+                int valueToWrite = 42;
+
+                client.Connect(AmsNetId.Local, 851);
+                //client.WriteValue("MAIN.nCounter", valueToWrite);
+                valueToRead = (int)client.ReadValue("MAIN.nCounter", typeof(int));
+                Console.WriteLine("Read value: " + valueToRead);
+            }
+            Console.WriteLine("Finished");
+        }
+       
+    }
+}
